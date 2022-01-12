48b0403a5c4f109c862dc05d66d971b6c694af94
Devi-512 <devipanchal753@gmail.com>
2022-01-12 14:48:29 +0530

    merging

--- 2.py
+++ 2.py
@@ -5,4 +5,14 @@ print("u r in standard: ", a-b)
 print("1st line")
 print("2nd line")
 print("3rd line")
+<<<<<<< Updated upstream
 print("4th line")
+=======
+print("4th line")
+
+print("5th line")
+print("6th line")
+print("7th line")
+
+print("8th line")
+>>>>>>> Stashed changes
